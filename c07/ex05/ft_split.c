646d74c4a7d14896133d2e0b93fad6b1c44a9efd
Elliott LOEVENBRUCK <eloevenb@e2r4p13.clusters.42paris.fr>
2022-07-19 18:02:02 +0200

    fini

--- c07/ex05/ft_split.c
+++ c07/ex05/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: eloevenb <eloevenb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/17 20:02:09 by eloevenb          #+#    #+#             */
-/*   Updated: 2022/07/17 20:59:17 by eloevenb         ###   ########.fr       */
+/*   Updated: 2022/07/19 17:57:01 by eloevenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,9 +31,6 @@ int	ft_cpy(char **dest, char *src, int size, int i)
 	return (1);
 }
 
-/*	
-	Returns the index of the first separator present in str
-*/
 int	ft_sep(char *str, char *charset)
 {
 	int	i;
@@ -54,12 +51,11 @@ int	ft_sep(char *str, char *charset)
 	return (-1);
 }
 
-char	**ft_split(char *str, char *charset)
+int	ft_nb_sep(char *str, char *charset)
 {
 	int		nb_sep;
 	int		sep;
 	int		prev_sep;
-	char	**strs;
 
 	sep = ft_sep(str, charset);
 	prev_sep = -1;
@@ -70,27 +66,35 @@ char	**ft_split(char *str, char *charset)
 		prev_sep = sep;
 		sep += 1 + ft_sep(str + sep + 1, charset);
 	}
+	return (nb_sep);
+}
+
+char	**ft_split(char *str, char *charset)
+{
+	int		nb_sep;
+	char	**strs;
+	int		sep;
+	int		p_sep;
+
+	nb_sep = ft_nb_sep(str, charset);
 	strs = (char **) malloc(nb_sep * sizeof(char *));
 	if (!strs)
 		return (0);
 	strs[nb_sep - 1] = 0;
 	nb_sep = 0;
-	prev_sep = -1;
+	p_sep = -1;
 	sep = ft_sep(str, charset);
-	while (sep != prev_sep)
+	while (sep != p_sep)
 	{
-		prev_sep = sep;
-		sep += 1 + ft_sep(str + prev_sep + 1, charset);
-		if (sep > 1 + prev_sep)
-		{
-			if (!ft_cpy(strs, str + prev_sep + 1, sep - prev_sep - 1, nb_sep))
+		p_sep = sep;
+		sep += 1 + ft_sep(str + p_sep + 1, charset);
+		if (sep > 1 + p_sep)
+			if (!ft_cpy(strs, str + p_sep + 1, sep - p_sep - 1, nb_sep++))
 				return (0);
-			nb_sep++;
-		}
 	}
 	return (strs);
 }
-
+/*
 int	main(int argc, char *argv[])
 {
 	char	**split;
@@ -107,8 +111,4 @@ int	main(int argc, char *argv[])
 		}
 	}
 	return (0);
-}
-
-
-
-
+}*/
